9d0e40a4c5ed7ed30effb4ed0db530bd29262eb3
vitozs <94878000+vitozs@users.noreply.github.com>
2023-03-27 20:25:26 -0300

    Trabalhando no layout 2, adicionando seções e etc.

--- scripts/misc.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $('iframe').on('load', function() {
-        $("iframe").contents().find("#document html body #player .vp-target").css("border", "9px solid yellow");
-    }); 
-});
